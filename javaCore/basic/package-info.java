a33700182b8cea198ef295c0568cde6e14942eb7
toaihang <toaiphan86@gmail.com>
2021-06-03 21:28:06 +0700

    unit hybrid-pom-nopcommerce

---
+++ javaCore/basic/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author TOAI-HANG
+ *
+ */
+package basic;
---
+++ testcases/facebook/web/users/Register.java
@@ -0,0 +1,19 @@
+package facebook.web.users;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Register {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
