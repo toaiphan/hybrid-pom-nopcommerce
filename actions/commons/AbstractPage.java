7433d7f377125325efdc7494f967ab931d465493
ps <ps@DESKTOP-UVLCEG8>
2021-06-04 17:59:23 +0700

    Update AbstractPage

--- actions/commons/AbstractPage.java
+++ actions/commons/AbstractPage.java
@@ -1,5 +1,14 @@
 package commons;
+//khi viet ham can chu y 5 van de : 
+//1.ham duoc su dung co chuc nang gi,
+//2.tham so truyen vao la gi
+//3. neu co tham so truyen vao thi define bien dai dien cho gia tri cua tha so
+//4.kieu tra ve cua ham trong == kieu tra ve cua ham ngoai
+//5.neu co tra ve thi can return
 
+//trong nay se viet tat cac cac ham common, de su dung lai
 public class AbstractPage {
-
+	
+	
+	
 }
--- javaCore/basic/Topic_34_Access_Modifier.java
+++ javaCore/basic/Topic_34_Access_Modifier.java
@@ -4,4 +4,8 @@ public class Topic_34_Access_Modifier {
 
 }
 //giong nhu phan quyen truy cap
-// nam trong nguyen tac dong goi , cho phep 1 class co the truy cap vao du lieu 1 class khac hay ko
+// Access_Modifier nam trong nguyen tac dong goi , cho phep 1 class co the truy cap vao du lieu 1 class khac hay ko
+//1.private : trong class
+//2.Default : 1 + trong package ( class khac trong package co the dung duoc)
+//3.Protected : 2 + ngoai package ( nhung phai la class con)
+//4.Public : dung o tat ca moi noi
