9d133d8696b839fe41ee233df4b8926a3f980de8
ps <ps@DESKTOP-UVLCEG8>
2021-06-04 14:17:04 +0700

    complete Topic_33_Elipse_Tip_Convention_OOP

---
+++ javaCore/basic/Topic_33_Eclipse_Tip_Convention_OOP.java
@@ -0,0 +1,31 @@
+//                           Eclipse TIP
+//Cltr + D : xoa dong/khoi lenh 
+//Ctrl + Shift+F : format lai code cho dep
+//Ctrl+shift+O : them/xoa thu vien
+//F3: di vao than ham hoac bien
+//Ctrl+shit+C hoac / : comment
+//ctrl+space : show lai suggest
+
+//move code: mui ten len xuong +alt
+//ctrl+- : zoom
+// ngoai ra co the tu setup 1 so cai : nhu duplicate code, colase all...
+
+//							Java Convention (quy uoc dat ten)
+
+//1. ten ham + ten bien : camelCase
+//2. ten testcace Under_Score
+//3. class name :PascaCase hoac Under_Score (chi pho bien cho testcase)
+//4. sau khi dung xong 1 class => xoa het comment+ print
+
+//							OOP ( HUONG DOI TUONG)
+//1. Encapsulation : dong goi cac ham/bien lai vao 1 class de cac class khac ke thua/su dung,che dau su khoi tao cua doi tuong
+// access modifier : pham vi truy cap, cho phep 1 class khac co the truy cap vap thuoc tinh/phuong thuc cua class nay hay ko
+//2.Inheritance :Ke thua .1 class con co the su dung thuoc tinh/phuong thuc cua class cha chia se cho no
+//3. Da hinh(poli : 1 ham co the co nhieu hanh vi,trang thai khac nhau tuy vao ngu canh
+//vi du co nhieu ham cung ten , nhung khac tham so truyen vao
+//4.TRUU tuong: abstract class  va interface ( se hoc sau)
+package basic;
+
+public class Topic_33_Eclipse_Tip_Convention_OOP {
+
+}
