397c745d55e81cb3397cad96b6c999971aaa0e50
ps <ps@DESKTOP-UVLCEG8>
2021-06-04 15:05:29 +0700

    Create topic 34

---
+++ javaCore/basic/Topic_34_Access_Modifier.java
@@ -0,0 +1,7 @@
+package basic;
+
+public class Topic_34_Access_Modifier {
+
+}
+//giong nhu phan quyen truy cap
+// nam trong nguyen tac dong goi , cho phep 1 class co the truy cap vao du lieu 1 class khac hay ko
