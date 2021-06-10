0b1156f4c31d624a1a4e51c49d0e3ac2dd7350d0
toaihang <toaiphan86@gmail.com>
2021-06-10 17:04:30 +0700

    update TC_01

--- actions/pageObjects/RegisterPageObject.java
+++ actions/pageObjects/RegisterPageObject.java
@@ -9,20 +9,86 @@ public class RegisterPageObject extends AbstractPage {
 	WebDriver driver;
 
 	// ham khoi tao ( contructor), khi new class nay len thi no se chay dau tien
-		// cung ten voi ten class
-		// gan driver toan cuc cho cuc bo
+	// cung ten voi ten class
+	// gan driver toan cuc cho cuc bo
 	// matching driver cho nhieu class
-		// chi can new 1 lan driver , nhung class khac gan lai
-		public RegisterPageObject(WebDriver driver) {
+	// chi can new 1 lan driver , nhung class khac gan lai
+	public RegisterPageObject(WebDriver driver) {
 
-			this.driver = driver;
-		}
+		this.driver = driver;
+	}
 
-		public void clickToGenderMaleRadioButton() {
-			waitToElementClickable(driver, RegisterPageUI.GENDER_MALE_RADIO);
-			clickToElement(driver, RegisterPageUI.GENDER_MALE_RADIO);
-			
-		} 
+	public void clickToGenderMaleRadioButton() {
+		waitToElementClickable(driver, RegisterPageUI.GENDER_MALE_RADIO);
+		clickToElement(driver, RegisterPageUI.GENDER_MALE_RADIO);
 
+	}
 
-} 
+	public void inputToFirstNameTextbox(String firstname) {
+		waitToElementVisible(driver, RegisterPageUI.FIRSTNAME_TEXTBOX);
+		sendKeyToElement(driver, RegisterPageUI.FIRSTNAME_TEXTBOX, firstname);
+	}
+
+	public void inputToLastNameTextbox(String lastname) {
+		waitToElementVisible(driver, RegisterPageUI.LASTNAME_TEXTBOX);
+		sendKeyToElement(driver, RegisterPageUI.LASTNAME_TEXTBOX, lastname);
+	}
+
+	public void selectDayDropdown(String day) {
+		waitToElementClickable(driver, RegisterPageUI.DAY_DROPDOWN);
+		selectItemInDropdown(driver, RegisterPageUI.DAY_DROPDOWN, day);
+
+	}
+
+	public void selectMonthDropdown(String month) {
+		waitToElementClickable(driver, RegisterPageUI.MONTH_DROPDOWN);
+		selectItemInDropdown(driver, RegisterPageUI.MONTH_DROPDOWN, month);
+
+	}
+
+	public void selectYearDropdown(String year) {
+		waitToElementClickable(driver, RegisterPageUI.YEAR_DROPDOWN);
+		selectItemInDropdown(driver, RegisterPageUI.YEAR_DROPDOWN, year);
+
+	}
+
+	public void inputToEmailTextbox(String email) {
+		waitToElementVisible(driver, RegisterPageUI.EMAIL_TEXTBOX);
+		sendKeyToElement(driver, RegisterPageUI.EMAIL_TEXTBOX, email);
+
+	}
+
+	public void inputToCompanyTextbox(String companyName) {
+		waitToElementVisible(driver, RegisterPageUI.COMPANY_TEXTBOX);
+		sendKeyToElement(driver, RegisterPageUI.COMPANY_TEXTBOX, companyName);
+	}
+
+	public void inputToPasswordTextbox(String password) {
+		waitToElementVisible(driver, RegisterPageUI.PASSWORD_TEXTBOX);
+		sendKeyToElement(driver, RegisterPageUI.PASSWORD_TEXTBOX, password);
+	}
+
+	public void inputToConfirmPasswordTextbox(String password) {
+		waitToElementVisible(driver, RegisterPageUI.CONFIRM_PASSWORD_TEXTBOX);
+		sendKeyToElement(driver, RegisterPageUI.CONFIRM_PASSWORD_TEXTBOX, password);
+	}
+
+	public void clickToRegisterButton() {
+		waitToElementClickable(driver, RegisterPageUI.REGISTER_BUTTON);
+		clickToElement(driver, RegisterPageUI.REGISTER_BUTTON);
+
+	}
+
+	public String getRegisteredSuccessMessage() {
+		waitToElementVisible(driver, RegisterPageUI.REGISTERED_SUCCESS_MESSAGE);
+		return getElementText(driver, RegisterPageUI.REGISTERED_SUCCESS_MESSAGE);
+
+	}
+
+	public void clickToLogoutLink() {
+		waitToElementClickable(driver, RegisterPageUI.LOGOUT_LINK);
+		clickToElement(driver, RegisterPageUI.LOGOUT_LINK);
+		
+	}
+
+}
--- testcases/com/nopcommerce/users/Level_01_Register_Login.java
+++ testcases/com/nopcommerce/users/Level_01_Register_Login.java
@@ -45,12 +45,6 @@ public class Level_01_Register_Login extends AbstractPage {
 
 		// khoi tao implicit wait 30s
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		// khoi tao abtractPage
-
-		// mo trang demo.nopcommerce.com
-		// driver.get("https://demo.nopcommerce.com/");
-
-		// mo URl su dung ham common
 		driver.get("https://demo.nopcommerce.com/");
 
 		// tao bo du lieu (email se +4 so random)
@@ -68,91 +62,29 @@ public class Level_01_Register_Login extends AbstractPage {
 		homePage.clickToRegisterLink();
 		registerPage = new RegisterPageObject(driver);
 		registerPage.clickToGenderMaleRadioButton();
-		
-
-		// click vao register
-//		driver.findElement(By.className("ico-register")).click();
-//
-//		// click vao male
-//		driver.findElement(By.id("gender-male")).click();
-//
-//		// sendkeys vao firstname
-//		driver.findElement(By.id("FirstName")).sendKeys(firstname);
-//
-//		// sendkeys vao lastname
-//		driver.findElement(By.id("LastName")).sendKeys(lastname);
-//
-//		// khoi tao select cho birthday
-//		select = new Select(driver.findElement(By.name("DateOfBirthDay")));
-//		// chon birthday ,su dung selecByvisible
-//		select.selectByVisibleText("8");
-//		// khoi tao select cho Birth month
-//		select = new Select(driver.findElement(By.name("DateOfBirthMonth")));
-//		// chon month ,su dung selecByvisible
-//		select.selectByVisibleText("June");
-//		// khoi tao select cho year
-//		select = new Select(driver.findElement(By.name("DateOfBirthYear")));
-//
-//		// chon year ,su dung selecByvisible
-//		select.selectByVisibleText("1995");
-//
-//		// sendkeys vao email
-//		driver.findElement(By.id("Email")).sendKeys(email);
-//
-//		// sendkeys vao company name
-//		driver.findElement(By.id("Company")).sendKeys(companyName);
-//
-//		// sendkeys vao pass
-//		driver.findElement(By.id("Password")).sendKeys(password);
-//
-//		// sendkeys vao confirm pass
-//		driver.findElement(By.id("ConfirmPassword")).sendKeys(password);
-//
-//		// click register
-//		driver.findElement(By.id("register-button")).click();
-//		// verify da dang ki thanh cong
-//		Assert.assertEquals(driver.findElement(By.className("result")).getText(), "Your registration completed");
-//		// click logout
-//		driver.findElement(By.className("ico-logout")).click();
+		registerPage.inputToFirstNameTextbox(firstname);
+		registerPage.inputToLastNameTextbox(lastname);
+		registerPage.selectDayDropdown("10");
+		registerPage.selectMonthDropdown("August");
+		registerPage.selectYearDropdown("1995");
+		registerPage.inputToEmailTextbox(email);
+		registerPage.inputToCompanyTextbox(companyName);
+		registerPage.inputToPasswordTextbox(password);
+		registerPage.inputToConfirmPasswordTextbox(password);
+		registerPage.clickToRegisterButton();
+		Assert.assertEquals(registerPage.getRegisteredSuccessMessage(), "Your registration completed");
+		registerPage.clickToLogoutLink();
+		homePage = new HomePageObject(driver);
 
 	}
 
 	// @Test
 	public void TC_02_Login() {
 
-		// click login
-		driver.findElement(By.className("ico-login")).click();
-		// send email+pass
-		driver.findElement(By.id("Email")).sendKeys(email);
-		driver.findElement(By.id("Password")).sendKeys(password);
-
-		// click vao login
-		driver.findElement(By.xpath("//button[contains(text(),'Log in')]")).click();
-
-		// verify : my account displayed,log out displayed
-		Assert.assertTrue(driver.findElement(By.className("ico-account")).isDisplayed());
-		Assert.assertTrue(driver.findElement(By.className("ico-logout")).isDisplayed());
-
 	}
 
 	// @Test
 	public void TC_03_View_My_Account() {
-
-		// click vao my account
-		driver.findElement(By.className("ico-account")).click();
-		// kiem tra male gender da duoc check roi
-		driver.findElement(By.id("gender-male")).isSelected();
-		// verify cac thong tin trung voi thoi tin da dang ki
-		Assert.assertEquals(driver.findElement(By.id("FirstName")).getAttribute("value"), firstname);
-		Assert.assertEquals(driver.findElement(By.id("LastName")).getAttribute("value"), lastname);
-		select = new Select(driver.findElement(By.name("DateOfBirthDay")));
-		Assert.assertEquals(select.getFirstSelectedOption().getText(), "8");
-		select = new Select(driver.findElement(By.name("DateOfBirthMonth")));
-		Assert.assertEquals(select.getFirstSelectedOption().getText(), "June");
-		select = new Select(driver.findElement(By.name("DateOfBirthYear")));
-		Assert.assertEquals(select.getFirstSelectedOption().getText(), "1995");
-		Assert.assertEquals(driver.findElement(By.id("Email")).getAttribute("value"), email);
-		Assert.assertEquals(driver.findElement(By.id("Company")).getAttribute("value"), companyName);
 
 	}
 
